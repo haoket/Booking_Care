99f99f581fbcbbede83c364cb4c13a9b1c03b7c3
Ket Hao <haoketys@gmail.com>
2022-12-16 17:17:31 +0700

    them chuc nang them, sua, xoa nguoi dung

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
+
 
 
 
@@ -18,6 +20,13 @@ let initWebRoutes = (app) => {
     router.get('/delete-crud', homeController.deleteCRUD);
 
 
+
+
+    router.post('/api/login', userController.handleLogin)
+    router.get('/api/get-all-users', userController.handleGetAllUsers)
+    router.post('/api/create-new-user', userController.handleCreateNewUser)
+    router.put('/api/edit-user', userController.handleEditUser)
+    router.delete('/api/delete-user', userController.handleDeleteUser)
     return app.use("/", router);
 
 }
